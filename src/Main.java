38cd59cb9c61b715c395651f22cd7cf2f66f58f9
joonlee <96dalee@gmail.com>
2025-06-04 10:11:45 +0900

    commit about the &&,||

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-            System.out.println("first java");
-            System.out.println("first java");
+        int age = 20;
+        boolean hasID = true;
+
+        if(age >= 18 && hasID) {
+            System.out.println("접속가능.");
+        }
+
+        boolean isWeekend = false;
+        boolean isHoliday = true;
+
+        if (isWeekend || isHoliday) {
+            System.out.println("쉴수있습니다.");
+        }
+
+        boolean isRaining = true;
+        if(!isRaining) {
+            System.out.println("그냥 나가.");
+        } else {
+            System.out.println("우산 챙겨.");
+        }
     }
 }
